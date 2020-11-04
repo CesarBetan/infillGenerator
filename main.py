8cba6ca85518426253ff91139931e6d0e968a6d6
Cesar Betancourt <betancourt@David-MacPro.local>
2020-11-03 21:21:00 -0600

    Get min and max region of each layer

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import readSlicedGcode as rsg 
 
 # Split from layers 4 through (n-4)
@@ -39,6 +41,43 @@ def splitLayers(layers, numLayers):
         res2.append(aux)
     return bottom, res1, res2, top
 
+# Get a lists of lists containing the [minX, minY, maxX, maxY]
+def getPrintingSpace(layers):
+    sizeLayers = len(layers)
+    spaces = np.empty((sizeLayers, 0)).tolist()
+    minimax = []
+    for i in range(sizeLayers):
+        l = layers[i]
+        aux = []
+        inner = True
+        # Get G commands for the inner layer
+        for layer in l:
+            if inner:
+                if ";TYPE:WALL-OUTER" not in layer:
+                    aux.append(layer)
+                else:
+                    layers[i] = aux
+                    inner = False
+            else: continue
+        # Get values for x and y for the layer
+        matching = [s for s in layers[i] if "X" in s]
+        x_ = []
+        y_ = []
+        for match in matching:
+            m = match.split( )
+            aux_x = [s for s in m if "X" in s]
+            aux_y = [s for s in m if "Y" in s]
+            x_.append(aux_x[0])
+            y_.append(aux_y[0])
+        # Get the [minX, minY, maxX, maxY] for each layer
+        for i in range(len(x_)):
+            x_[i] = float(x_[i].split('X')[1])
+            y_[i] = float(y_[i].split('Y')[1])
+        res = [min(x_), min(y_), max(x_), max(y_)]
+        minimax.append(res)
+    return minimax
+        
+
 
 def main():
     filename = "./inGcode/cube_20mm.gcode"
@@ -52,8 +91,9 @@ def main():
     eof = r.parsed[3]
     # It returns two list of lists containing the code printed before the infill and after the infill
     bottom, sLayer1, sLayer2, top = splitLayers(layers, numLayers)
-    print(top)
-    print(len(top))
+    # Get the min and max printing space for each layer
+    printSpace = getPrintingSpace(sLayer1)
+    print(printSpace)
 
 if __name__ == "__main__":
     main()
